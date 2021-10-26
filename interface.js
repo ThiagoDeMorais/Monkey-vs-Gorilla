86fad3393c69e46c91e8577066963ab3aa007b3f
Thigo De Morais <demoraisthiago63@gmail.com>
2021-10-26 20:54:44 -0300

    inserção, de acordo com o turno, dos icones das classes m e g no tabuleiro

--- interface.js
+++ interface.js
@@ -1,4 +1,19 @@
 const iconsOfPlayers = document.querySelectorAll('.image');
+const squares = document.querySelectorAll('.square');
+
+function refreshSquare(square){
+    const newClass = board[square.id]
+    square.classList.add(newClass);
+}
+
+squares.forEach(square =>{
+    square.addEventListener('click', event => {
+        const position = event.target.id;
+        handleTurn(position);
+        refreshSquare(event.target)
+        console.log(board);
+    })
+})
 
 function removePopUpPicker(){
     const popup = document.querySelector('.popup-wrapper');
@@ -6,12 +21,16 @@ function removePopUpPicker(){
 }
 
  const getTurnPlayer =  event => {
-    assignTurnPlayer(event.target.id);
+     if(event.target.id === 'monkey'){
+        assignTurnPlayer(0);
+     }else{
+        assignTurnPlayer(1);
+     }
     console.log(turnPlayer);
     removePopUpPicker();
 }
 
 
-iconsOfPlayers.forEach(imagePlayer =>{
-    imagePlayer.addEventListener('click', getTurnPlayer)
+iconsOfPlayers.forEach(iconPlayer =>{
+    iconPlayer.addEventListener('click', getTurnPlayer)
 })
--- rules.js
+++ rules.js
@@ -1,4 +1,27 @@
+let board = ['','','',
+             '','','',
+             '','',''];
+
+
 let turnPlayer = 0;
+let playerSymbols = ['m','g'];
+
+function handleTurn(position){
+    if(board[position]!=='') {
+       return;
+   }
+
+   board[position] = playerSymbols[turnPlayer];
+   console.log(turnPlayer );
+
+   if(turnPlayer !== 0){
+      turnPlayer = 0;
+   }else{
+      turnPlayer = 1;
+   }
+
+
+}
 
 const assignTurnPlayer = id => {
      turnPlayer = id;
